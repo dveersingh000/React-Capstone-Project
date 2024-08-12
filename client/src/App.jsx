42bc34e0f25f885060c60afff34f8974fb281fd2
Dharamveer <dveersingh000@gmail.com>
2024-08-13 01:57:07 +0530

    Implemented Routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-
-import './App.css'
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
-  
   return (
-    <>  
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import styles from "./RegisterPage.module.css";
+export default function RegisterPage() {
+  return <div className={styles.container}>RegisterPage</div>
+}
